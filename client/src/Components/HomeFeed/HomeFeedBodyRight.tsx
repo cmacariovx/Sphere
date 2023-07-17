abe5e843102877945aaa6ab528c00951ab3b6260
Carlos Macario <cmacariovx@gmail.com>
2023-07-17 17:14:32 -0400

    home feed profile

--- client/src/Components/HomeFeed/HomeFeedBodyRight.tsx
+++ client/src/Components/HomeFeed/HomeFeedBodyRight.tsx
@@ -11,6 +11,42 @@ function HomeFeedBodyRight() {
                     <p className={styles.profileName}>Carlos Macario</p>
                     <p className={styles.profileTitle}>Founder of Sphere</p>
                 </div>
+                <div className={styles.explore}>
+                    <p className={styles.exploreHeader}>Explore</p>
+                    <div className={styles.exploreGroup}>
+                        <p className={styles.exploreGroupHeader}>Recent</p>
+                        <div className={styles.exploreItem}>
+                            <i className={`${styles.exploreItemIcon} fa-solid fa-hashtag`}></i>
+                            <p className={styles.exploreItemText}>softwareengineering</p>
+                        </div>
+                        <div className={styles.exploreItem}>
+                            <i className={`${styles.exploreItemIcon} fa-solid fa-user-group`}></i>
+                            <p className={styles.exploreItemText}>Software Engineering</p>
+                        </div>
+                    </div>
+                    <div className={styles.exploreGroup}>
+                        <p className={styles.exploreGroupHeader}>For you</p>
+                        <div className={styles.exploreItem}>
+                            <i className={`${styles.exploreItemIcon} fa-solid fa-hashtag`}></i>
+                            <p className={styles.exploreItemText}>softwareengineering</p>
+                        </div>
+                        <div className={styles.exploreItem}>
+                            <i className={`${styles.exploreItemIcon} fa-solid fa-user-group`}></i>
+                            <p className={styles.exploreItemText}>Software Engineering</p>
+                        </div>
+                    </div>
+                    <div className={styles.exploreGroup}>
+                        <p className={styles.exploreGroupHeader}>Following</p>
+                        <div className={styles.exploreItem}>
+                            <i className={`${styles.exploreItemIcon} fa-solid fa-hashtag`}></i>
+                            <p className={styles.exploreItemText}>softwareengineering</p>
+                        </div>
+                        <div className={styles.exploreItem}>
+                            <i className={`${styles.exploreItemIcon} fa-solid fa-user-group`}></i>
+                            <p className={styles.exploreItemText}>Software Engineering</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
